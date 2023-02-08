76c6794b3ca263c339ac872a64fea62aa62f0aba
Lukas Oppermann <lukasoppermann@github.com>
2023-02-08 14:06:12 +0100

    trigger push to main

--- .github/templates/default-release-note.ts
+++ .github/templates/default-release-note.ts
@@ -14,12 +14,12 @@ export const render: Template = ({ fileInfo, triggeredBy, description, changes }
 ---
 
 # Published ${new Date(fileInfo.timestamp).toLocaleDateString("en-US", {})  /*! transform to real data */}
-by ${triggeredBy.handle}\n\n
+by ${triggeredBy.handle}<br /><br />
 
-${`${description}\n\n` ?? ''}
+${`${description}<br /><br />` ?? ''}
 
 ${hasComponentUpdate(changes) ? `## Component updates` : ''}
-\
+<br>
 ${changes.createdComponents?.map(item => `### [${item.name} (added)](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
 
   ![Thumbnail for ${item.name}](${item.thumbnailUrl})
@@ -32,7 +32,7 @@ ${changes.modifiedComponents?.map(item => `### [${item.name} (updated)](https://
 
 ${changes.deletedComponents?.map(item => `### [${item.name} (deleted)](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
 `).join("\n")}
-\
+<br>
 ${hasStyleUpdate(changes) ? `## Style updates` : ''}\
 ${changes.createdStyles?.map(item => `- [${item.name} (added)](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
 `).join("\n")}
