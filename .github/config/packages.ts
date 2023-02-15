80ea3721f7b7da926cdbb8e65b836f220245ea58
Lukas Oppermann <lukasoppermann@github.com>
2023-02-15 12:00:52 +0100

    add figma prefix to packages

--- .github/config/packages.ts
+++ .github/config/packages.ts
@@ -1,10 +1,10 @@
 export const packages: Record<string, { name: string, template: string }> = {
   'GCvY3Qv8czRgZgvl1dG6lp': {
-    name: '@primer/web',
+    name: '@primer/figma-web',
     template: 'default-release-note'
   },
   'BJ95AjraesmRCWsKA013GS': {
-    name: '@primer/brand',
+    name: '@primer/figma-brand',
     template: 'default-release-note'
   },
   'B5XPE8IwGPIZDAvN7jqWqx': {
@@ -12,15 +12,15 @@ export const packages: Record<string, { name: string, template: string }> = {
     template: 'default-release-note'
   },
   'Y2xJLFBrU7yyiDLlEkQXcF': {
-    name: '@primer/interfaces',
+    name: '@primer/figma-interfaces',
     template: 'default-release-note'
   },
   '9vhDNyQpOono1vgi0qSViZ': {
-    name: '@primer/primitives-light',
+    name: '@primer/figma-primitives-light',
     template: 'default-release-note'
   },
   'EQ1q7R5qtk6gqJLxEx0QHf': {
-    name: '@primer/primitives-dark',
+    name: '@primer/figma-primitives-dark',
     template: 'default-release-note'
   },
   'HD7FUvOEHLtWvWuhu1AUaJ': {
