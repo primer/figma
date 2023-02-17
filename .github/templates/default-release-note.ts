e04c67f8c2ffb4c7c6cc90dcc003b25d6de489ce
Lukas Oppermann <lukasoppermann@github.com>
2023-02-17 13:28:28 +0100

    updated format for changelog

--- .github/templates/default-release-note.ts
+++ .github/templates/default-release-note.ts
@@ -20,25 +20,25 @@ ${`${description}   ` ?? ''}
 
 ${hasComponentUpdate(changes) ? `### Component updates` : ''}
 
-${changes.createdComponents?.map(item => `#### [${item.name} (added)](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
+${changes.createdComponents?.map(item => `#### [${item.name} [added]](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
 
   ![Thumbnail for ${item.name}](${item.thumbnailUrl})
 `).join("\n")}
 
-${changes.modifiedComponents?.map(item => `#### [${item.name} (updated)](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
+${changes.modifiedComponents?.map(item => `#### [${item.name} [updated]](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
   
   ![Thumbnail for ${item.name}](${item.thumbnailUrl})
 `).join("\n")}
 
-${changes.deletedComponents?.map(item => `#### [${item.name} (deleted)](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
+${changes.deletedComponents?.map(item => `#### [${item.name} [deleted]](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
 `).join("\n")}
 
 ${hasStyleUpdate(changes) ? `### Style updates
 ` : ''}
-${changes.createdStyles?.map(item => `- [${item.name} (added)](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
+${changes.createdStyles?.map(item => `- [${item.name} [added]](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
 `).join("\n")}
-${changes.modifiedStyles?.map(item => `- [${item.name} (updated)](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
+${changes.modifiedStyles?.map(item => `- [${item.name} [updated]](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
 `).join("\n")}
-${changes.deletedStyles?.map(item => `- [~${item.name}~ (deleted)](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
+${changes.deletedStyles?.map(item => `- [~${item.name}~ [deleted]](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
 `).join("\n")}
 `
