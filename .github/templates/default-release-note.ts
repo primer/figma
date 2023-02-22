d50a12fafcff49c251aad0a53698070863bd9cca
Lukas Oppermann <lukasoppermann@github.com>
2023-02-22 23:28:28 +0100

    update template

--- .github/templates/default-release-note.ts
+++ .github/templates/default-release-note.ts
@@ -1,11 +1,20 @@
-import type { Template } from './Template'
+import type { ComponentItemData, ComponentSetData, StyleItemData, Template } from './Template'
 
-const hasStyleUpdate = (changes) => {
-  return changes?.createdStyles?.length > 0 || changes?.modifiedStyles?.length > 0 || changes?.deletedStyles?.length > 0
-}
+const hasStyleUpdate = (changes: {
+  componentSets: ComponentSetData[];
+  styles: StyleItemData[];
+}) => changes?.styles?.length > 0
 
-const hasComponentUpdate = (changes) => {
-  return changes?.createdComponents?.length > 0 || changes?.modifiedComponents?.length > 0 || changes?.deletedComponents?.length > 0
+const hasComponentUpdate = (changes: {
+  componentSets: ComponentSetData[];
+  styles: StyleItemData[];
+}) => changes?.componentSets?.length > 0
+
+const renderName = (item: StyleItemData | ComponentItemData) => {
+  if (item.state === 'deleted') {
+    return `~${item.name}~ (${item.state})`
+  }
+  return `${item.name} (${item.state})`
 }
 
 export const render: Template = ({ fileInfo, triggeredBy, description, changes }) => `
@@ -13,32 +22,21 @@ export const render: Template = ({ fileInfo, triggeredBy, description, changes }
 "${fileInfo.package}": minor
 ---
 
-- Published ${new Date(fileInfo.timestamp).toLocaleDateString("en-US", {})  /*! transform to real data */}
+Published ${new Date(fileInfo.timestamp).toLocaleDateString("en-GB", {})  /*! transform to real data */}  
 by ${triggeredBy.handle}${"\n\n"}    
-  
+   
 ${`${description}   ` ?? ''}
-  
+   
 ${hasComponentUpdate(changes) ? `### Component updates` : ''}
 
-${changes.createdComponents?.map(item => `- [**${item.name} [added]**](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
-
-![Thumbnail for ${item.name}](${item.thumbnailUrl})
-`).join("\n")}
-
-${changes.modifiedComponents?.map(item => `- [**${item.name} [updated]**](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
-  
-![Thumbnail for ${item.name}](${item.thumbnailUrl})
-`).join("\n")}
-
-${changes.deletedComponents?.map(item => `- [**${item.name} [deleted]**](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
+${changes.componentSets?.map(item => `#### [**${item.setName}**](https://www.figma.com/file/${fileInfo.fileKey}?node-id=${item.setId})
+   
+${item.components?.map(component => `- [${renderName(component)}](https://www.figma.com/file/${component.file_key}?node-id=${component.node_id})  
+![Thumbnail for ${component.name}](${component.thumbnailUrl})`).join("\n\n")}
 `).join("\n")}
 
 ${hasStyleUpdate(changes) ? `### Style updates
 ` : ''}
-${changes.createdStyles?.map(item => `- [${item.name} [added]](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
-`).join("\n")}
-${changes.modifiedStyles?.map(item => `- [${item.name} [updated]](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
-`).join("\n")}
-${changes.deletedStyles?.map(item => `- [~${item.name}~ [deleted]](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
+${changes.styles?.map(item => `- [${renderName(item)}](https://www.figma.com/file/${item.file_key}?node-id=${item.node_id})
 `).join("\n")}
 `
