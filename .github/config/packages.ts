8af8c7eb7eb887d2de480fb73bdf5139ad8c97ee
Lukas Oppermann <lukasoppermann@github.com>
2024-07-15 10:20:07 +0200

    Update packages.ts

--- .github/config/packages.ts
+++ .github/config/packages.ts
@@ -6,25 +6,5 @@ export const packages: Record<string, { name: string, template: string }> = {
   'BJ95AjraesmRCWsKA013GS': {
     name: '@primer/figma-brand',
     template: 'default-release-note'
-  },
-  'B5XPE8IwGPIZDAvN7jqWqx': {
-    name: '@primer/primitives',
-    template: 'default-release-note'
-  },
-  'Y2xJLFBrU7yyiDLlEkQXcF': {
-    name: '@primer/figma-interfaces',
-    template: 'default-release-note'
-  },
-  '9vhDNyQpOono1vgi0qSViZ': {
-    name: '@primer/figma-primitives-light',
-    template: 'default-release-note'
-  },
-  'EQ1q7R5qtk6gqJLxEx0QHf': {
-    name: '@primer/figma-primitives-dark',
-    template: 'default-release-note'
-  },
-  'HD7FUvOEHLtWvWuhu1AUaJ': {
-    name: '@primer/figma-api-test',
-    template: 'default-release-note'
-  },
+  }
 }
